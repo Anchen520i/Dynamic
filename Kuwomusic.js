fb3d515455a97cd41b4537d9066396238d570070
安晨&XY <54392045+Anchen520i@users.noreply.github.com>
2023-04-16 14:07:09 +0800

    解锁酷我音乐；

--- Kuwomusic.js
+++ Kuwomusic.js
@@ -1,3 +1,15 @@
+/* 
+脚本功能：完美解锁酷我音乐
+脚本作者：king
+下载地址：https://is.gd/CWqKMX
+更新时间：2022.8.16
+脚本频道：https://t.me/yqc_123
+问题反馈：https://t.me/yqc_777
+使用声明：⚠️⚠️⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+
+代码重构:这版是 最完美！！！的酷我解锁
+当前版本:10.2.8
 [rewrite_local]
 # 完美解锁酷我音乐
 ^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/nameking77/Qx/main/rewrite/kw.js
