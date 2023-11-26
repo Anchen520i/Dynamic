da558f16a4b07eaf0ea3bb8230a93ded6420d62f
安晨@Anchen <54392045+Anchen520i@users.noreply.github.com>
2023-11-26 21:36:19 +0800

    Create Notability.js

---
+++ Notability.js
@@ -0,0 +1,28 @@
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "570001184068302",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "2099-09-09T09:09:09.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
+  }
+};
+
+$done({body : JSON.stringify(chxm1023)});
